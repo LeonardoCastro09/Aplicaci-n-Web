65d0a816414e8a0e813cd0c45a9dbdc31938e1dd
LeonardoCastro09 <alcastroc@correo.udistrital.edu.co>
2022-12-12 17:41:47 -0500

    V 1.0.3

--- Programa.py
+++ Programa.py
@@ -1,1 +1,36 @@
 #Importar bibliotecas
+
+from flask import Flask
+from flask_sqlalchemy import *
+
+#Crear objeto clase Flask
+
+app=Flask(__name__)
+
+#Configurar el acceso a la base de datos
+#URL de la base de datos
+
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///estudiantes.sqlite3"
+
+#Crear la base de datos
+
+db=SQLAlchemy(app)
+
+#Definir tablas de la base de datos
+
+class estudiante(db.Model):
+    #Definir una llave primaria (identificador unico)
+    id=db.Column("id_estudiante",db.Integer, primary_key=True)
+    
+    #Definir columnas de la tabla
+    nombre=db.Column(db.String(50))
+    codigo=db.Column(db.String(12))
+    
+    def __init__(self, nombre, codigo):
+        self.nombre=nombre
+        self.codigo=codigo
+        
+#Crear objeto de la clase estudiante
+
+with app.app_context():
+    db.create_all()
